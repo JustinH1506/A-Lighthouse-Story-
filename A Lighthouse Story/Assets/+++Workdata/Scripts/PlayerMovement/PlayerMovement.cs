6938e85fedf7a71be6fe42b30994358fecad3336
Justin <justinherrmann34@gmail.com>
2024-02-22 10:41:08 +0100

    Added TextMeshPro

---
+++ A Lighthouse Story/Assets/+++Workdata/Scripts/PlayerMovement/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Scripts
+    
+    private PlayerControllerMap _playerControllerMap;
+    
+    #endregion
+    
+    #region Variables
+
+    [SerializeField] private float moveSpeed;
+
+    #endregion
+    
+    
+}
