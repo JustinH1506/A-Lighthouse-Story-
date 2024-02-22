a4ebbfd7e4e5a835bb5347a2d36fbdb35f22c73a
Justin <justinherrmann34@gmail.com>
2024-02-22 11:49:32 +0100

    Added PlayerMovement, Camera Follow, Camera Colliding with walls.

--- A Lighthouse Story/Assets/+++Workdata/Scripts/PlayerMovement/PlayerMovement.cs
+++ A Lighthouse Story/Assets/+++Workdata/Scripts/PlayerMovement/PlayerMovement.cs
@@ -1,7 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
     #region Scripts
@@ -12,9 +16,52 @@ public class PlayerMovement : MonoBehaviour
     
     #region Variables
 
+    private Rigidbody rb;
+    
+    [Header("Movement")]
+    [Tooltip("Speed to make the Player Move (Has to be negative.)")]
+    [Space(5)]
     [SerializeField] private float moveSpeed;
+
+    private float inputX, inputZ;
 
     #endregion
     
-    
+    #region Methods
+    private void Awake()
+    {
+        _playerControllerMap = new PlayerControllerMap();
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        _playerControllerMap.Enable();
+
+        _playerControllerMap.Player.Move.performed += Move;
+        _playerControllerMap.Player.Move.canceled += Move;
+    }
+
+    private void OnDisable()
+    {
+        _playerControllerMap.Disable();
+        
+        _playerControllerMap.Player.Move.performed -= Move;
+        _playerControllerMap.Player.Move.canceled -= Move;
+
+    }
+
+    public void FixedUpdate()
+    {
+        rb.velocity = new Vector3(inputX * moveSpeed, rb.velocity.y, inputZ * moveSpeed);
+    }
+
+    private void Move(InputAction.CallbackContext context)
+    {
+        inputX = context.ReadValue<Vector3>().x;
+        
+        inputZ = context.ReadValue<Vector3>().z;
+    }
+    #endregion
 }
