51ed835cbdffaf78f73464e9df4311cde38be14b
Justin <justinherrmann34@gmail.com>
2024-03-01 11:06:29 +0100

    Added "Enemy" finding Player

--- A Lighthouse Story/Assets/+++Workdata/Scripts/Editor/FieldOfViewEditor.cs
+++ A Lighthouse Story/Assets/+++Workdata/Scripts/Editor/FieldOfViewEditor.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.XR;
 
 [CustomEditor(typeof(FieldOfView))]
 public class FieldOfViewEditor : Editor
@@ -12,5 +13,16 @@ public class FieldOfViewEditor : Editor
       FieldOfView fow = (FieldOfView)target;
       Handles.color = Color.white;
       Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+      Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+      Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+      
+      Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+      Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+
+      Handles.color = Color.red;
+      foreach (Transform visibleTarget in fow.visibleTargets)
+      {
+         Handles.DrawLine(fow.transform.position, visibleTarget.position);
+      }
    }
 }
