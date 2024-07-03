84dba4ed61e87af453772e451188329616494d9a
Justin <justinherrmann34@gmail.com>
2024-07-03 06:33:38 +0200

    Added Keyframes in animation for sounds and started sound script for these animations

--- A Lighthouse story HDRP/Assets/+++Workdata/Scripts/Player/Scripts/States.cs
+++ A Lighthouse story HDRP/Assets/+++Workdata/Scripts/Player/Scripts/States.cs
@@ -1,13 +1,22 @@
 using System;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class States : PlayerBase
 {
-    [SerializeField] private LayerMask sand, grass;
+    [SerializeField] private LayerMask sand, grass, wood;
+
+    private bool isSand;
+    
+    private bool isWood;
+    
+    private bool isGrass;
 
     private float waitUntilLookIdle;
+    
+    List<AudioClip> sandStepList, grassStepList, woodStepList;
 
     private void Start()
     {
@@ -15,6 +24,24 @@ public class States : PlayerBase
         {
             waitUntilLookIdle = Random.Range(3f, 5f);
         }
+        
+        /*sandStepList.Add(MusicManager.Instance.sandStepA);
+        sandStepList.Add(MusicManager.Instance.sandStepB);
+        sandStepList.Add(MusicManager.Instance.sandStepC);
+        sandStepList.Add(MusicManager.Instance.sandStepD);
+        sandStepList.Add(MusicManager.Instance.sandStepE);
+        
+        sandStepList.Add(MusicManager.Instance.grassStepA);
+        sandStepList.Add(MusicManager.Instance.grassStepB);
+        sandStepList.Add(MusicManager.Instance.grassStepC);
+        sandStepList.Add(MusicManager.Instance.grassStepD);
+        sandStepList.Add(MusicManager.Instance.grassStepE);
+        
+        sandStepList.Add(MusicManager.Instance.woodStepA);
+        sandStepList.Add(MusicManager.Instance.woodStepB);
+        sandStepList.Add(MusicManager.Instance.woodStepC);
+        sandStepList.Add(MusicManager.Instance.woodStepD);
+        sandStepList.Add(MusicManager.Instance.woodStepE);*/
     }
 
     private void Update()
@@ -36,13 +63,51 @@ public class States : PlayerBase
     {
         if (other.gameObject.layer == sand)
         {
-            Debug.Log("Bitch");
+            isSand = true;
+
+            isGrass = false;
+
+            isWood = false;
+            
             MusicManager.Instance.PlayAmbience(MusicManager.Instance.beachAmbience, 1f);
         }
 
         if (other.gameObject.layer == grass)
         {
+            isGrass = true;
+            
+            isSand = false;
+
+            isWood = false;
+            
             MusicManager.Instance.PlayAmbience(MusicManager.Instance.forestAmbience, 1f);
+        }
+        
+        if (other.gameObject.layer == wood)
+        {
+            isWood = true;
+            
+            isSand = false;
+
+            isGrass = false;
+        }
+    }
+
+    public void StepAudio()
+    {
+        if (isSand)
+        {
+            MusicManager.Instance.PlaySFX(sandStepList[Random.Range(0, 5)]);
+        }
+        
+        if (isSand)
+        {
+            MusicManager.Instance.PlaySFX(grassStepList[Random.Range(0, 5)]);
+        }
+        
+        if (isSand)
+        {
+            MusicManager.Instance.PlaySFX(woodStepList[Random.Range(0, 5)]);
         }
     }
 }
